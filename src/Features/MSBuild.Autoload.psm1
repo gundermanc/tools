a7a0fcf6f5ad7e2a391452774c60f077b89db83a
Christian Gunderman <gundermanc@gmail.com>
2019-05-03 22:03:07 -0700

    Improvements to error list.

--- src/Features/MSBuild.Autoload.psm1
+++ src/Features/MSBuild.Autoload.psm1
@@ -1,31 +1,35 @@
 # MSBuild tools: Aliases and tools for working with MSBuild
 # By: Christian Gunderman
 
+function EnsureMSBuild
+{
+    if (!(Get-Command msbuild -ErrorAction SilentlyContinue))
+    {
+        Throw "Path to MSBuild.exe not found. Run vsget to select an instance and vscmd [id] to select a VS dev environment to import."
+    }
+}
+
 # Builds a project using typical settings.
 function Start-MSBuild($project)
 {
-    & msbuild.exe /r /t:build /m $project
-}
-
-# Builds a project with a clickable error list that launches in VS.
-function Start-MSBuildWithErrorList($project)
-{
-    & msbuild.exe /t:build /m $project | & "$Global:FeatureDir\MSBuildErrorList.ps1"
+    EnsureMSBuild
+    & msbuild.exe /r /t:build /m $project | & "$Global:FeatureDir\MSBuildErrorList.ps1"
 }
 
 # Cleans a project.
 function Start-MSClean($project)
 {
-    & msbuild.exe /t:clean /m $project
+    EnsureMSBuild
+    & msbuild.exe /t:clean /m $project | & "$Global:FeatureDir\MSBuildErrorList.ps1"
 }
 
 # Restores a project.
 function Start-MSRestore($project)
 {
-    & msbuild.exe /t:restore /m $project
+    EnsureMSBuild
+    & msbuild.exe /t:restore /m $project | & "$Global:FeatureDir\MSBuildErrorList.ps1"
 }
 
 New-Alias -Name msbbuild -Value Start-MSBuild
-New-Alias -Name msbebuild -Value Start-MSBuildWithErrorList
 New-Alias -Name msbclean -Value Start-MSClean
 New-Alias -Name msbrestore -Value Start-MSRestore
