c87585ac1b8d063abf88238e1a96c67461a14b4e
Christian Gunderman <chgund@microsoft.com>
2019-06-07 13:38:33 -0700

    Initialize submodule on build.

--- Build.bat
+++ Build.bat
@@ -3,6 +3,9 @@
 :: Powershell REPL + Tools -> Installer Batch Script Self Extractor Build Script
 :: By: Christian Gunderman
 
+git submodule init
+git submodule update
+
 :: Build.bat must be run within the src directory.
 cd external\clown-car
 
