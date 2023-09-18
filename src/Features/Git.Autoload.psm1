c720b1fe7d3dc88a48ffada5334a608e56d5a32f
Sandy Armstrong <sanfordarmstrong@gmail.com>
2023-09-18 11:06:32 -0700

    Allow Get-GitRoot to work with worktrees
    Worktrees don't have a `.git` directory.

--- src/Features/Git.Autoload.psm1
+++ src/Features/Git.Autoload.psm1
@@ -8,18 +8,29 @@ function Set-GitRoot()
 
 function Get-GitRoot()
 {
-    $currentDirectory = Convert-Path .
-    $lastIndex = $currentDirectory.LastIndexOf('\')
-
-    while ($lastIndex -ne -1)
+    if (Get-Command git 2>$null)
     {
-        if (Test-Path (Join-Path $currentDirectory ".git") -PathType Container)
+        $root =  . git rev-parse --show-toplevel 2>$null
+        if ($LASTEXITCODE -eq 0)
         {
-            return $currentDirectory
+            return Convert-Path $root
         }
-
-        $currentDirectory = $currentDirectory.Substring(0, $lastIndex)
+    }
+    else
+    {
+        $currentDirectory = Convert-Path .
         $lastIndex = $currentDirectory.LastIndexOf('\')
+
+        while ($lastIndex -ne -1)
+        {
+            if (Test-Path (Join-Path $currentDirectory ".git") -PathType Container)
+            {
+                return $currentDirectory
+            }
+
+            $currentDirectory = $currentDirectory.Substring(0, $lastIndex)
+            $lastIndex = $currentDirectory.LastIndexOf('\')
+        }
     }
 
     Write-Host -ForegroundColor Yellow "Not a git repository"
